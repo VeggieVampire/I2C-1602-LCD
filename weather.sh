4aac6f1c4b58fd8ca5e0f585f4350d1498efd119
Nick Farrow <VeggieVampire@users.noreply.github.com>
2016-01-22 00:13:51 -0600

    Adding weather to your LCD
    change 'tulsa%2C%20ok' to your city or town to get the weather. to run it type ./print.py $(./weather.sh)

---
+++ weather.sh
@@ -0,0 +1,1 @@
+curl "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22tulsa%2C%20ok%22)&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"|sed -n '32p'
