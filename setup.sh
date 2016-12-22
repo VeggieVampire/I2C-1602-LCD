deb943fdf8b49d8d778f2165b7e4e0f933c868c0
Nick Farrow <VeggieVampire@users.noreply.github.com>
2016-12-22 00:25:48 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-sudo apt-get install i2c-tools
-sudo apt-get install python-smbus
+sudo apt-get -Y install i2c-tools
+sudo apt-get -Y install python-smbus
 sudo echo "i2c-bcm2708">>/etc/modules
 sudo echo "i2c-dev">>/etc/modules
 echo "You need to reboot"
